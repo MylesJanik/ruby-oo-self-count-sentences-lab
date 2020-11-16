0bf1d0d0e2c55f80d152d826c9c72739c87544ec
Myles <mylesjanik@gmail.com>
2020-11-15 18:47:27 -0800

    commit tree 3d283a649ef56e9c09cbc1e95ec26d7ddda78611

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    temp_array_1 = self.split(/[.!?]/)
+    temp_array_2 = []
 
+    temp_array_1.each do |x|
+      if x.length != 0
+        temp_array_2 << x
+      end
+    end
+    temp_array_2.count
   end
 end
